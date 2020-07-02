5e0171397ba3f12bacf33d5f54478ddac3a68f81
lmluo <lmluomeng1992@163.com>
2020-07-02 15:04:57 +0800

    easyexcle批量处理并入库

--- springcloud-tools/springcloud-easyexcel/src/main/java/com/rebote/domain/DemoDataListener.java
+++ springcloud-tools/springcloud-easyexcel/src/main/java/com/rebote/domain/DemoDataListener.java
@@ -2,6 +2,7 @@ package com.rebote.domain;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.rebote.service.DemoDataService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,17 +15,23 @@ import java.util.List;
  */
 public class DemoDataListener extends AnalysisEventListener<DemoData> {
 
-    List<DemoData> result = new ArrayList<>();
+    List<DemoData> resultList = new ArrayList<>();
+
+    private DemoDataService demoDataService;
+
+    public DemoDataListener(DemoDataService demoDataService) {
+        this.demoDataService = demoDataService;
+    }
 
     @Override
     public void invoke(DemoData data, AnalysisContext context) {
         // 每解析完一行数据之后，都会调用该方法
-        result.add(data);
+        resultList.add(data);
     }
 
     @Override
     public void doAfterAllAnalysed(AnalysisContext context) {
         // 解析完所有的数据之后，会调用该方法
-
+        demoDataService.insertBatch(resultList);
     }
 }
--- springcloud-tools/springcloud-easyexcel/src/main/java/com/rebote/service/impl/DemoDataServiceImpl.java
+++ springcloud-tools/springcloud-easyexcel/src/main/java/com/rebote/service/impl/DemoDataServiceImpl.java
@@ -1,10 +1,22 @@
 package com.rebote.service.impl;
 
+import com.rebote.domain.DemoData;
+import com.rebote.service.DemoDataService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
- * @ClassName: 业务逻辑
+ * @ClassName: 数据处理业务逻辑层
  * @description:
  * @author: luomeng
  * @time: 2020/7/2 14:06
  */
-public class DemoDataServiceImpl {
+@Service
+public class DemoDataServiceImpl implements DemoDataService {
+
+    @Override
+    public void insertBatch(List<DemoData> list) {
+        // TODO数据库持久层
+    }
 }
