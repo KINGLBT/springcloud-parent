c249b2e950717d3c29eed47e8296c9fd5a28dfcc
luomeng <lmluomeng1992@163.com>
2020-09-17 11:53:04 +0800

    springcloud-config-server 基于git ssh方式访问

---
+++ springcloud-config/springcloud-config-server-ssh/src/main/java/com/rebote/GitConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.rebote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ProjectName: springcloud-parent
+ * @Package: com.rebote
+ * @ClassName: GitConfigServerApplication
+ * @Author: lmluo
+ * @Description: 机遇SSH方式git仓库配置中心
+ * @Date: 2020/9/17 11:46
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class GitConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfigServerApplication.class, args);
+    }
+}
